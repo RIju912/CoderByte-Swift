6c2d39d00feb0d36b5645bee515f1a0f1f2dcb36
Banerjee, Subhodip <Subhodip.Banerjee@Honeywell.com>
2018-06-19 20:44:05 +0530

    First commit.

---
+++ First-Reverse/FirstReverse.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+/***
+ 
+ Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+ 
+ Use the Parameter Testing feature in the box below to test your code with different arguments.
+ 
+***/
+import UIKit
+
+func FirstReverse(_ str: String) -> String {
+    
+    
+    return String(str.reversed())
+    
+}
+
+print(FirstReverse("I am a boy"));
+
