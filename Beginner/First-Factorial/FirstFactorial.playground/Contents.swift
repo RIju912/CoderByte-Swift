33164f20232a91ed772bb94743c1a35cd74a636d
Banerjee, Subhodip <Subhodip.Banerjee@Honeywell.com>
2018-06-19 23:56:40 +0530

    .

---
+++ Beginner/First-Factorial/FirstFactorial.playground/Contents.swift
@@ -0,0 +1,28 @@
+/*
+ Challenge
+ 
+ Using the Swift language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+ 
+ Sample Test Cases
+ Input:4
+ Output:24
+ 
+ Input:8
+ Output:40320
+ */
+
+import UIKit
+
+func FirstFactorial(_ num: Int) -> Int {
+    
+    if num == 0{
+        return 1
+    }
+    
+    return num * FirstFactorial(num-1)
+    
+}
+
+// keep this function call here
+print(FirstFactorial(8))
+
